f8afbd81b3c7d5fe623f46e40f56453a3cc2bc73
Greys1512 <greylisagudelov15@gmail.com>
2026-08-22 17:34:44 -0500

    feat: implementacion de micro servicios en el backend

--- backend/services/auth-service/src/app.js
+++ backend/services/auth-service/src/app.js
@@ -6,6 +6,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Auth Service funcionando correctamente'
+    });
+});
+
+app.use('/api/auth', require('./routes/auth.routes'));
+
 app.use(require('./middlewares/errorHandler'));
 
 module.exports = app;
--- backend/services/notifications-service/src/app.js
+++ backend/services/notifications-service/src/app.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const cors = require('cors');
+
 require('dotenv').config();
 
 const app = express();
+
 app.use(cors());
+
 app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Notifications Service funcionando correctamente'
+    });
+});
+
+app.use('/api/notifications', require('./routes/notifications.routes'));
 
 app.use(require('./middlewares/errorHandler'));
 
